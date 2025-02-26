d901f053c2174785c07124a85b4b243c2f9e8ea0
LiamJPOS <paledeepened@gmail.com>
2025-02-26 15:38:03 +0000

    Implement structure of menu items

--- src/home.js
+++ src/home.js
@@ -1,8 +1,22 @@
-export function constructHome (parent, handlers = []) {
+export function constructHome(parentDiv, handlers = []) {
+    parentDiv.classList.add("home-layout");
+    parentDiv.classList.add("bg-img");
+
     const homeHeader = document.createElement("h2");
     homeHeader.classList.add("main__home-header");
     homeHeader.textContent = "We pride ourself on having a nice looking restaurant"
-    parent.appendChild(homeHeader)
+    parentDiv.appendChild(homeHeader)
 
-    
+    const homeSubtext = document.createElement("p");
+    homeSubtext.classList.add("main__home-subtext");
+    homeSubtext.textContent = "The food is alright too, I guess... Like it aint amazing, but it'll do. Our chef is kind of mid tho so don't expect much.";
+    parentDiv.appendChild(homeSubtext);
+
+    const homeAboutBtn = document.createElement("button");
+    homeAboutBtn.classList.add("main__home-about-btn");
+    homeAboutBtn.textContent = "About";
+    homeAboutBtn.addEventListener("click", () => {
+        console.log("About btn clicked")
+    })
+    parentDiv.appendChild(homeAboutBtn);
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
 import "./styles.css";
 import { constructHome } from "./home";
+import {menu}  from './restaurant_menu'
 
-class TabController {
+console.log(menu.starters)
+
+class Controller {
     constructor() {
         //Cashe DOM
+        this.sidebarBtn = document.getElementById("sidebarBtn");
         this.main = document.getElementById("main");
+        this.sidebar = document.getElementById("sidebar");
+        this.sidebarCloseBtn = document.getElementById("sidebarCloseBtn");
+
+        //Event Listeners
+        this.sidebarBtn.addEventListener("click", this.onSidebarBtn.bind(this));
+        this.sidebarCloseBtn.addEventListener("click", this.onSidebarCloseBtn.bind(this));
     }
 
-    clearChildren (parent) {
+    #clearChildren (parent) {
         const children = [...parent.children];
         for (const child of children) {
             parent.removeChild(child);
         }
     }
 
-    renderHome () {
-        this.clearChildren(this.main);
+    renderHome() {
+        this.#clearChildren(this.main);
         constructHome(this.main);
     }
+
+    onSidebarBtn() {
+        this.sidebar.classList.add("active")
+    }
+
+    onSidebarCloseBtn() {
+        this.sidebar.classList.remove("active")
+    }
+
 }
 
-const tabController = new TabController();
-window.addEventListener("load", () => {
-    tabController.renderHome();
-});
+const controller = new Controller();
+// window.addEventListener("load", () => {
+//     controller.renderHome();
+// });
+
 
